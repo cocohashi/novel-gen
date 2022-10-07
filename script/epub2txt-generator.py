1371c585c79ac1ddc15bb2c7029f0e664cc40990
Hasier Ollo <hasier.ollo@magnet.coop>
2022-10-07 16:42:43 +0200

    Refactor with Black code formatter

--- script/epub2txt-generator.py
+++ script/epub2txt-generator.py
@@ -12,51 +12,51 @@ import argparse
 from epub2txt import epub2txt
 
 # Environment Variables
-EPUB_DIR_PATH = 'books/epub'
-TXT_DIR_PATH = 'books/txt'
+EPUB_DIR_PATH = "books/epub"
+TXT_DIR_PATH = "books/txt"
 
 # Define Argument Parser
 def get_args():
-        
-    parser = argparse.ArgumentParser(description=\
-             f"""Generates .txt format files from .epub files""")
-    parser.add_argument('--workdir', 
-                        type=str,
-                        help='Workdir path')
+
+    parser = argparse.ArgumentParser(
+        description=f"""Generates .txt format files from .epub files"""
+    )
+    parser.add_argument("--workdir", type=str, help="Workdir path")
     return parser.parse_args()
-    
+
+
 def generate_paths(workdir):
     print(workdir)
     input_path = os.path.join(workdir, EPUB_DIR_PATH)
     output_path = os.path.join(workdir, TXT_DIR_PATH)
     print(input_path)
     return input_path, output_path
-    
+
 
 def epub2txt_from_file(input_path, output_path):
-    try:    
+    try:
         os.chdir(output_path)
         output_list = [f for f in glob.glob("*.txt")]
         os.chdir(input_path)
         input_list = [f for f in glob.glob("*.epub")]
         files = list(set(input_list) - set(output_list))
-        file_names = list(map(lambda x: x.split('.epub')[0],files))
-        print(f'file_names: {file_names}')
+        file_names = list(map(lambda x: x.split(".epub")[0], files))
+        print(f"file_names: {file_names}")
         for file_name in file_names:
             try:
-                file_text = epub2txt(f'{file_name}.epub')
-                with open(os.path.join(output_path, f'{file_name}.txt'), 'w') as f:
+                file_text = epub2txt(f"{file_name}.epub")
+                with open(os.path.join(output_path, f"{file_name}.txt"), "w") as f:
                     f.write(file_text)
-                    print(f'[FILE]: {file_name}.txt Done!')
+                    print(f"[FILE]: {file_name}.txt Done!")
                     f.close()
             except Exception as e:
-                print(f'[FILE-ERROR]: {file_name}.txt failed.\n{e}\n')
+                print(f"[FILE-ERROR]: {file_name}.txt failed.\n{e}\n")
         return True
     except Exception as e:
-        print(f'[GENERATOR-ERROR]: {e}')
-    
+        print(f"[GENERATOR-ERROR]: {e}")
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     args = get_args()
     input_path, output_path = generate_paths(args.workdir)
     epub2txt_from_file(input_path, output_path)
